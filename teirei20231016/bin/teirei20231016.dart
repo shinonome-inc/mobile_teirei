c5ab7807d3ef8694afee7e2bee11353fefc20d68
KobayashiYoh <y.kobayashi@4nonome.com>
2023-10-16 21:05:42 +0900

    create project

---
+++ teirei20231016/bin/teirei20231016.dart
@@ -0,0 +1,5 @@
+import 'package:teirei20231016/teirei20231016.dart' as teirei20231016;
+
+void main(List<String> arguments) {
+  print('Hello world: ${teirei20231016.calculate()}!');
+}
---
+++ teirei20231016/test/teirei20231016_test.dart
@@ -0,0 +1,8 @@
+import 'package:teirei20231016/teirei20231016.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
