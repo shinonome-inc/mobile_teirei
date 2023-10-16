793b5a7328fffccba8431edccd852652d5efb86c
KobayashiYoh <y.kobayashi@4nonome.com>
2023-10-16 21:06:14 +0900

    不要なコードを削除

--- teirei20231016/bin/teirei20231016.dart
+++ teirei20231016/bin/teirei20231016.dart
@@ -1,5 +1,3 @@
-import 'package:teirei20231016/teirei20231016.dart' as teirei20231016;
-
 void main(List<String> arguments) {
-  print('Hello world: ${teirei20231016.calculate()}!');
+  print('Hello world: !');
 }
--- teirei20231016/test/teirei20231016_test.dart
+++ teirei20231016/test/teirei20231016_test.dart
@@ -1,8 +1,1 @@
-import 'package:teirei20231016/teirei20231016.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+void main() {}
