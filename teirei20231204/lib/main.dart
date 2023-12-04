dafefb177753b3551a13c5f0852eea69831d0eb9
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2023-12-04 21:48:48 +0900

    しまねっこペイの項目を追加

--- teirei20231204/lib/main.dart
+++ teirei20231204/lib/main.dart
@@ -47,9 +47,80 @@ class MyHomePage extends StatelessWidget {
           ),
           centerTitle: true,
           title: const Text(
-            'rregion PAY',
+            'region PAY',
             style: TextStyle(
               fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        body: Center(
+          child: Container(
+            width: 280.0,
+            height: 320.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(32.0),
+              color: Colors.blue,
+            ),
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(16.0),
+                    topRight: Radius.circular(16.0),
+                  ),
+                  child: Image.asset('assets/images/simanekko-pay.png'),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 16.0),
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '「ご縁も、美肌も、島根から。」し…',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'しまねっこ ペイ',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '残高',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            '0',
+                            style: TextStyle(
+                              fontSize: 36.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            'pt',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ),
